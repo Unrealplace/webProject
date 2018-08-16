283c6c58c8737febcaa3378d3df49092c5d786c1
LiYang <oliverleemail@gmail.com>
2018-08-16 18:30:27 +0800

    1.学习mysql 数据库

---
+++ mysql/mysql_use.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liyang
+ * Date: 2018/8/16
+ * Time: 上午11:35
+ */
+
+/**
+ * 建立数据库链接
+ * 调用函数的时候忽略警告，在函数名前加一个@
+ */
+   $succed =  @mysqli_connect('127.0.0.1','root','12345678','liyang_db');
+
+   if ($succed){
+       echo '链接数据库成功';
+   }else {
+       echo '链接数据库失败';
+   }
+
+   $result = mysqli_query($succed,'SELECT * FROM user;');
+
+   $data = mysqli_fetch_assoc($result);
+
+   var_dump($data);
+
+   mysqli_close($succed);
+
+?>
