208c18c8a0f4b4c08b7743d372add82450e8a420
liyang <liyang@adnonstop.com>
2018-08-27 23:01:20 +0800

    ajax封装

---
+++ ajax/xml.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liyang
+ * Date: 2018/8/25
+ * Time: 下午4:38
+ */
+header('Content-Type:application/xml');
+
+
+?>
+
+<person>
+    <age>19</age>
+    <name>hello</name>
+    <work>mimi</work>
+
+</person>
